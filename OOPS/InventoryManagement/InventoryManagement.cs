38a45ee1cdae165507fe44d0c60747703205aac0
Imransk7768 <imransk7768@gmail.com>
2022-04-25 23:58:14 +0530

    [Imran] Added: Dispalying PriceRates

---
+++ OOPS/InventoryManagement/InventoryManagement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace OOPS
+{
+    public class InventoryManagement
+    {
+        public void DisplayInventory(string filePath)
+        {
+            StreamReader reader = new StreamReader(filePath);
+            {
+                var json = reader.ReadToEnd();
+                var inventory = JsonConvert.DeserializeObject<List<InventoryData>>(json);
+                Console.WriteLine("Name" + "\t" + "Weight" + "\t" + "PricePerKg" + "\t" + "TotalPrice");
+                foreach(var data in inventory)
+                {
+                    Console.WriteLine(data.Name + "\t" + data.Weight + "\t" + data.PricePerKg + "\t\t" + (data.Weight * data.PricePerKg));
+                }
+            }
+        }
+    }
+}
---
+++ OOPS/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OOPS 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(">>>>  Welcome To Inventory  <<<<");
+            const string INVENTORY_DATA_PATH = @"H:\Assignments\Object Oriented Programs\OOPS\InventoryManagement\Inventory.json";
+            InventoryManagement InData = new InventoryManagement();
+
+            Random random = new Random();
+            bool check = true;
+            while (check)
+            {
+                Console.WriteLine("1.Displat Data");
+                switch (Convert.ToInt32(Console.ReadLine()))
+                {
+                    case 1:
+                        InData.DisplayInventory(INVENTORY_DATA_PATH);
+                        break;
+                    case 2:
+                        Console.WriteLine("Exited");
+                        break;
+                }
+
+            }
+        }
+    }
+}
